37b48fbc6ce87a2aeb8468717ccb383677cb9561
Nachi4265 <hvirgen@my.yearupunited.org>
2025-10-22 23:08:24 -0400

    Some examples in main

--- src/main/java/com/pluralsight/Deck.java
+++ src/main/java/com/pluralsight/Deck.java
@@ -6,17 +6,21 @@ import java.util.Collections;
 public class Deck {
     private ArrayList<Card>cards = new ArrayList<>();
 
+    //Constructor for a deck
     public Deck(){
         String[]suits = {"Heart","Diamond","Club","Spade"};
         String[]values = {"2","3","4","5","6","7","8","9","10","J","K","Q","A"};
 
         for(String suit: suits){
             for(String value:values){
+
                 Card card = new Card(suit,value);
                 cards.add(card);
+
             }
         }
     }
+
 
 
     public void shuffle(){
@@ -24,14 +28,15 @@ public class Deck {
     }
 
 
-
     public Card deal(){
         if(cards.size()>0){
             return cards.remove(0);
-        }else {
+        }else{
             return null;
         }
     }
+
+
 
     public int getSize(){
         return cards.size();
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -6,15 +6,36 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Player 1 what is your name: ");
-        String player1Name = scanner.next();
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Player 1 what is your name: ");
+//        String player1Name = scanner.next();
+//
+//        System.out.print("Player 2 what is your name: ");
+//        String player2Name = scanner.next();
+//
+//        System.out.println("Welcome "+ player1Name + " and " + player2Name);
+//        System.out.println("I will now deal you both 2 cards");
 
-        System.out.print("Player 2 what is your name: ");
-        String player2Name = scanner.next();
+        //created deck
+        Deck MorgDeck = new Deck();
 
-        System.out.println("Welcome "+ player1Name + " and " + player2Name);
-        System.out.println("I will now deal you both 2 cards");
+        //deal 1 card from deck
+        MorgDeck.deal();
+        System.out.println(MorgDeck.getSize());
+
+        Card card1 = MorgDeck.deal();
+        card1.flip();
+        System.out.println(card1.getValue() + " of " + card1.getSuit());
+
+        Card card2 = MorgDeck.deal();
+        card2.flip();
+        System.out.println(card2.getValue() + " of " + card2.getSuit());
+
+        MorgDeck.shuffle();
+
+        Card card3 = MorgDeck.deal();
+        card3.flip();
+        System.out.println(card3.getValue() + " of " + card3.getSuit());
 
     }
 }
